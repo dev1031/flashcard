2647e55b9d84f820effe67186ff88c2d827bbd37
Dherendra Dev <42056549+dev1031@users.noreply.github.com>
2025-12-05 19:26:13 +0530

    Added Circuit Breaker and Actuator

---
+++ src/main/java/com/example/flashcard/controllers/CircuitBreakerController.java
@@ -0,0 +1,35 @@
+package com.example.flashcard.controllers;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.apache.juli.logging.Log;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class CircuitBreakerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/circuit-break")
+    @CircuitBreaker(name = "circuitbreaker",fallbackMethod = "getCircuitError")
+    public String circuitBreaker() throws Exception{
+        LOG.info("Calling local method🐷🐷🐷🐷");
+        String URL = "http://localhost:8080/book-service";
+        String response = restTemplate.getForObject(URL, String.class);
+        return response;
+    }
+
+    public String getCircuitError(Throwable throwable){
+        LOG.warn("Called CircuitBreaker Method🧨🧨🧨");
+        return "Something is wrong with book-service";
+    }
+}
